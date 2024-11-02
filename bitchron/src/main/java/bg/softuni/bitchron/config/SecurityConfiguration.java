bfea76b06caf15d51d1971594d9c65acbfdb3b7e
DaHuska <aboyanov@abv.bg>
2024-11-02 10:17:41 +0200

    Initial commit

--- bitchron/src/main/java/bg/softuni/bitchron/config/SecurityConfiguration.java
+++ bitchron/src/main/java/bg/softuni/bitchron/config/SecurityConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -47,5 +49,10 @@ public class SecurityConfiguration {
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
         return new BitChronUserDetailsService(userRepository);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 }
--- bitchron/src/main/java/bg/softuni/bitchron/service/impl/BitChronUserDetailsService.java
+++ bitchron/src/main/java/bg/softuni/bitchron/service/impl/BitChronUserDetailsService.java
@@ -1,9 +1,13 @@
 package bg.softuni.bitchron.service.impl;
 
+import bg.softuni.bitchron.entity.UserEntity;
 import bg.softuni.bitchron.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
 
 public class BitChronUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
@@ -14,6 +18,17 @@ public class BitChronUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findByUsername(username)
+                .map(this::map)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found!"));
+    }
 
+    private UserDetails map(UserEntity userEntity) {
+        return User
+                .withUsername(userEntity.getUsername())
+                .password(userEntity.getPassword())
+                .authorities(List.of()) // TODO: add roles
+                .build();
     }
 }
