14471a3823853cbe9866616a6fac8eb578c94adc
DaHuska <aboyanov@abv.bg>
2025-04-06 08:43:43 +0300

    Add thymeleaf field validation

--- src/main/java/bg/softuni/bitchron/web/UserController.java
+++ src/main/java/bg/softuni/bitchron/web/UserController.java
@@ -2,10 +2,16 @@ package bg.softuni.bitchron.web;
 
 import bg.softuni.bitchron.model.dto.UserRegisterDTO;
 import bg.softuni.bitchron.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -18,17 +24,31 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String login() {
-        return "login";
+    public ModelAndView login(ModelAndView modelAndView) {
+        modelAndView.setViewName("login");
+
+        return modelAndView;
     }
 
     @GetMapping("/register")
     public String register() {
+//        modelAndView.setViewName("register");
+
         return "register";
     }
 
     @PostMapping("/register")
-    public String register(UserRegisterDTO userRegisterDTO) {
+    public String register(@Valid  UserRegisterDTO userRegisterDTO,
+                           BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes
+                    .addFlashAttribute("userRegisterDTO", userRegisterDTO);
+            redirectAttributes
+                    .addFlashAttribute("org.springframework.validation.BindingResult.userRegisterDTO", bindingResult);
+        }
+
         userService.register(userRegisterDTO);
 
         return "redirect:/";
