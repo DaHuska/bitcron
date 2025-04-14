c35ada16cfa80c25f2aca6ccab3cddd70c057cba
DaHuska <aboyanov@abv.bg>
2025-04-14 22:59:59 +0300

    Add "create new product offer" page

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -3,3 +3,32 @@ let subMenu = document.getElementById("subMenu");
 function toggleMenu() {
     subMenu.classList.toggle("open-menu");
 }
+
+function displaySelectedImage(event, elementId) {
+    const selectedImage = document.getElementById(elementId);
+    const fileInput = event.target;
+
+    if (fileInput.files && fileInput.files[0]) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            selectedImage.src = e.target.result;
+        };
+
+        reader.readAsDataURL(fileInput.files[0]);
+    }
+}
+
+const sidebarIcon = document.querySelector(".aside_menu_button");
+const sidebar = document.querySelector(".mobile-menu");
+
+sidebarIcon.addEventListener('click', function() {
+    if (sidebarIcon.classList.contains("toggled")) {
+        sidebar.classList.remove("close");
+        sidebarIcon.classList.remove("toggled");
+        // TODO: fix animation
+    } else {
+        sidebar.classList.add("close");
+        sidebarIcon.classList.add("toggled");
+    }
+});
