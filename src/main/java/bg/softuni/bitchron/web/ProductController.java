9b66d4dd6c6eb5f74fd04ebb9dc6a4d4517d1929
DaHuska <aboyanov@abv.bg>
2025-04-18 09:05:59 +0300

    Refactor controller name

--- src/main/java/bg/softuni/bitchron/web/ProductController.java
+++ src/main/java/bg/softuni/bitchron/web/ProductController.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class NewProductController {
+public class ProductController {
     @GetMapping("/add-watch")
     public ModelAndView addWatch() {
         return new ModelAndView("add-watch");
     }
+
+
 }
