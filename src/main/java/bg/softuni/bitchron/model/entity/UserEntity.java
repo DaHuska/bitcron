a63d86367432258b09d6dedb88da05738b6e7051
DaHuska <aboyanov@abv.bg>
2025-05-17 16:44:54 +0300

    Feat: add user properties

--- src/main/java/bg/softuni/bitchron/model/entity/UserEntity.java
+++ src/main/java/bg/softuni/bitchron/model/entity/UserEntity.java
@@ -2,6 +2,7 @@ package bg.softuni.bitchron.model.entity;
 
 import bg.softuni.bitchron.model.validation.DateNotInTheFuture;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -12,11 +13,9 @@ import java.util.List;
 @Entity
 @Table(name = "users")
 public class UserEntity extends BaseEntity {
-    @NotBlank
     @Column(name = "first_name")
     private String firstName;
 
-    @NotBlank
     @Column(name = "last_name")
     private String lastName;
 
@@ -27,6 +26,25 @@ public class UserEntity extends BaseEntity {
     @NotBlank
     @Column
     private String password;
+
+    @Column
+    @Email
+    private String email;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String country;
+
+    @Column(name = "city_or_state")
+    private String cityOrState;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column
+    private String address;
 
     @NotNull
     @ManyToMany(fetch = FetchType.EAGER)
@@ -113,6 +131,66 @@ public class UserEntity extends BaseEntity {
 
     public UserEntity setModified(Date modified) {
         this.modified = modified;
+
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserEntity setEmail(String email) {
+        this.email = email;
+
+        return this;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public UserEntity setPhone(String phone) {
+        this.phone = phone;
+
+        return this;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public UserEntity setCountry(String country) {
+        this.country = country;
+
+        return this;
+    }
+
+    public String getCityOrState() {
+        return cityOrState;
+    }
+
+    public UserEntity setCityOrState(String cityOrState) {
+        this.cityOrState = cityOrState;
+
+        return this;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public UserEntity setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public UserEntity setAddress(String address) {
+        this.address = address;
 
         return this;
     }
