dddb3a240eeaa5fe145d289f99f69493c6a666f8
DaHuska <aboyanov@abv.bg>
2025-05-20 17:47:39 +0300

    Fix: remove unnecessary methods

--- src/main/java/bg/softuni/bitchron/repository/ProductRepository.java
+++ src/main/java/bg/softuni/bitchron/repository/ProductRepository.java
@@ -4,11 +4,7 @@ import bg.softuni.bitchron.model.entity.WatchEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface ProductRepository extends JpaRepository<WatchEntity, Long> {
-    List<WatchEntity> findAll();
-    Optional<WatchEntity> findById(Long id);
+
 }
